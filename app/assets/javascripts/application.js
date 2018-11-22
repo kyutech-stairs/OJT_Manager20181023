6b12c0f2255b4535fafb9fee8037040394e82f57
KITCAT <k.k963tensai@gmail.com>
2018-11-23 04:42:52 +0900

    hel

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,6 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery2
 //= require cocoon
 //= require rails-ujs
 //= require activestorage
