767c862cde70b88b3c26a4ab679e8484db9ced4c
KITCAT <k.k963tensai@gmail.com>
2018-11-22 22:32:00 +0900

    heroku1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_kanrisya!
+  config.assets.initialize_on_precompile = false
 
    protected
 
