d831a182ccb3a4be84340bf4d905bc77f0784b03
KITCAT <k.k963tensai@gmail.com>
2018-11-23 00:21:04 +0900

    heloo

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require nested_form_fields
 //= require jquery
 //= require jquery_ujs
+//= require nested_form_fields
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
